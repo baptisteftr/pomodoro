7cc6311a57bf4aca722071669597d025a64c2ae9
Baptiste Fortier <bfortier@solem.fr>
2024-02-19 16:44:24 +0100

    WIP
    - wip: Pomodoro aspect

--- Pomodoro/Features/Pomodoro/PomodoroHomeView.swift
+++ Pomodoro/Features/Pomodoro/PomodoroHomeView.swift
@@ -10,21 +10,23 @@ import SwiftUI
 struct PomodoroHomeView: View {
     @State var pomodoroViewModel: PomodoroViewModel = PomodoroViewModel()
     @State var rotation: Double = 90.0
-    @State var timesLeft: Double = 0.0
+    @State var timesLeft: Double = 45.0
     @State var startTimer: Bool = false
+    @State var translatedDate: String = ""
     
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
     var body: some View {
         NavigationStack {
             VStack {
                 ZStack {
+                    Circle()
+                        .stroke(style: StrokeStyle(lineWidth: 1))
+                        .foregroundStyle(Color.primary)
                     Clock1minCircleView()
                     Clock5minCircleView()
-                    Circle()
-                        .foregroundStyle(Color.primary.opacity(0.1))
                     Capsule()
                         .foregroundStyle(Color.accentColor)
-                        .frame(width: 90, height: 5)
+                        .frame(width: 90, height: 3)
                         .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .leading)
                         .padding(.horizontal)
                         .rotationEffect(.init(degrees: rotation))
@@ -38,15 +40,45 @@ struct PomodoroHomeView: View {
                         .shadow(radius: 5)
                 }
                 .frame(width: 200, height: 200, alignment: .center)
-                Button {
-                    timesLeft = 45
-                    rotation = valueToDegree(minutes: timesLeft, totalTime: 45)
-                } label: {
-                    Text("Tester la rotatio sans anim")
+                HStack {
+                    Button {
+                        timesLeft -= 5.0
+                        rotation = valueToDegree(minutes: timesLeft)
+                        translatedDate = formatTime(timesLeft)
+                    } label: {
+                        Image(systemName: "gobackward.5")
+                            .foregroundStyle(Color.white)
+                            .padding(.horizontal, 6)
+                            .padding(.bottom, 6)
+                            .padding(.top, 5)
+                            .background(Color.accentColor, in: Circle())
+                    }
+                    Text(translatedDate)
+                        .animation(nil, value: UUID())
+                        .padding(.horizontal)
+                        .padding(.vertical, 5)
+                        .frame(width: 100)
+                        .background {
+                            Capsule()
+                                .stroke(style: StrokeStyle(lineWidth: 1))
+                                .foregroundStyle(.accent)
+                        }
+                    Button {
+                        timesLeft += 5.0
+                        rotation = valueToDegree(minutes: timesLeft)
+                        translatedDate = formatTime(timesLeft)
+                    } label: {
+                        Image(systemName: "goforward.5")
+                            .foregroundStyle(Color.white)
+                            .padding(.horizontal, 6)
+                            .padding(.bottom, 6)
+                            .padding(.top, 5)
+                            .background(Color.accentColor, in: Circle())
+                    }
                 }
+                .padding(.bottom)
                 Button {
-                    timesLeft = 45
-                    rotation = valueToDegree(minutes: timesLeft, totalTime: 45)
+                    rotation = valueToDegree(minutes: timesLeft)
                     startTimer = true
                 } label: {
                     Text("Tester la rotation")
@@ -57,19 +89,36 @@ struct PomodoroHomeView: View {
             .onReceive(timer, perform: { _ in
                 
                 if startTimer {
-                    timesLeft -= 1
-                    rotation = valueToDegree(minutes: timesLeft, totalTime: 45)
-                }
-                if timesLeft < 0 {
-                    rotation = 90
-                    startTimer = false
+                    timesLeft -= 1/60
+                    translatedDate = formatTime(timesLeft)
+                    rotation = valueToDegree(minutes: timesLeft)
+                    if timesLeft < 0 {
+                        rotation = 90
+                        startTimer = false
+                    }
                 }
             })
+            .onAppear {
+                translatedDate = formatTime(timesLeft)
+                rotation = valueToDegree(minutes: timesLeft)
+            }
         }
     }
 }
 
-func valueToDegree(minutes: Double, totalTime: Double) -> Double {
+func formatTime(_ minutes: Double) -> String {
+    let hours = Int(minutes) / 60
+    let remainingMinutes = Int(minutes) % 60
+    let seconds = Int((minutes - Double(hours * 60 + remainingMinutes)) * 60)
+    
+    if hours > 0 {
+        return "\(hours):\(remainingMinutes):\(seconds)"
+    } else {
+        return "\(remainingMinutes):\(seconds)"
+    }
+}
+
+func valueToDegree(minutes: Double) -> Double {
     let angle = (minutes / 60) * 360.0 + 90
     
     return angle
